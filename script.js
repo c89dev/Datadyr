465dbd2c5e9765616bb31824cbe00b856e650fa1
c89dev <snarvei@live.com>
2025-08-20 13:58:13 +0200

    Init commit
    Init commit

---
+++ script.js
@@ -0,0 +1,80 @@
+        let currentPet;
+        let isDead = false;
+        let isHangry = false;
+        let isSad = false;
+        let health;
+        let hunger = 100;
+        let mood = 100;
+
+
+        function startGame(){
+            health = 100;
+            hunger = 100;
+            mood = 100;
+        }
+
+        function HangryScale(){
+            if (hunger < 50 && isDead != true){
+                isHangry = true;
+                document.getElementById('petImg').src="graphics/pet_hungry.png"
+            }
+            else if (hunger > 50 && mood < 200 && mood > 50){
+                isHangry = false;
+                document.getElementById('petImg').src="graphics/pet_neutral.png"
+            }
+        }
+
+        function HungerOverTime(){
+            if (hunger > 0){
+            hunger --;
+        }
+        else{
+            isDead = true;
+            document.getElementById('hungerDiv').innerText = "DEAD"
+            document.getElementById('petImg').src="graphics/pet_dead.png"
+        }
+        return hunger;
+        }
+
+        function HappyScale(){
+
+            if (mood < 50 && isDead != true){
+                isSad = true;
+                document.getElementById('petImg').src="graphics/pet_sad.png"
+            }
+            else if (mood > 150){
+                isSad = false;
+                document.getElementById('petImg').src="graphics/pet_happy.png"
+            }
+            return mood;
+        }
+
+        function MoodOverTime(){
+            if (mood > 0){
+            mood --;
+        }
+        // else{
+        //     isDead = true;
+        //     document.getElementById('hungerDiv').innerText = "DEAD"
+        //     document.getElementById('petImg').src="graphics/pet_dead.png"
+        // }
+        return mood;
+        }
+
+        function FeedPet(){
+            hunger = hunger + 20;
+        }
+
+        function ComfortPet(){
+            mood = mood + 20;
+        }
+
+        function refresh(){
+            if(isDead != true){
+                document.getElementById('moodDiv').innerText = MoodOverTime();
+                document.getElementById('moodDiv').innerText = HappyScale();
+                document.getElementById('hungerDiv').innerText = HungerOverTime();
+                document.getElementById('hangryDiv').innerText = HangryScale();
+
+            }
+        }
